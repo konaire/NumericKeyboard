54c9e044ea4437efca194727e4396dd157d5414d
Evgeny Eliseyev <eugvelis@gmail.com>
2018-02-10 22:42:34 +0300

    initial commit

---
+++ app/src/main/java/com/konaire/sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.konaire.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Evgeny Eliseyev on 10/02/2018.
+ */
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
